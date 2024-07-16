fa3563335107941d4dd6d73990ffc8fd567269dd
William C Bonner <wimbonner@hotmail.com>
2024-07-15 22:08:13 -0700

    list controller object details

--- main.cpp
+++ main.cpp
@@ -33,44 +33,74 @@
 
 GDBusConnection* con(NULL);
 
-static const gchar* bluez_adapter_get_property(const gchar* path, const char* name)
+static void bluez_property_value(const gchar* key, GVariant* value)
 {
-    GVariant* result;
-    GVariant* prop;
-    GError* error = NULL;
-    const gchar* address = NULL;
+    const gchar* type = g_variant_get_type_string(value);
 
-    result = g_dbus_connection_call_sync(con,
-        "org.bluez",
-        path,
-        "org.freedesktop.DBus.Properties",
-        "Get",
-        g_variant_new("(ss)", "org.bluez.Adapter1", name),
-        NULL,
-        G_DBUS_CALL_FLAGS_NONE,
-        -1,
-        NULL,
-        &error);
-    if (error != NULL)
-        return NULL;
+    g_print("\t%s : ", key);
+    switch (*type) {
+    case 's':
+        g_print("%s\n", g_variant_get_string(value, NULL));
+        break;
+    case 'b':
+        g_print("%d\n", g_variant_get_boolean(value));
+        break;
+    case 'u':
+        g_print("%d\n", g_variant_get_uint32(value));
+        break;
+    case 'a':
+        g_print("\n");
+        const gchar* uuid;
+        GVariantIter i;
+        g_variant_iter_init(&i, value);
+        while (g_variant_iter_next(&i, "s", &uuid))
+            g_print("\t\t%s\n", uuid);
+        break;
+    default:
+        g_print("Other\n");
+        break;
+    }
+}
 
-    g_variant_get(result, "(v)", &prop);
-    g_variant_unref(result);
-    return g_variant_get_string(prop, NULL);
+static void bluez_adapter_getall_property(GDBusConnection* con,
+    GAsyncResult* res,
+    gpointer data)
+{
+    GVariant* result = NULL;
+    GMainLoop* loop = NULL;
+
+    loop = (GMainLoop*)data;
+
+    result = g_dbus_connection_call_finish(con, res, NULL);
+    if (result == NULL)
+        g_print("Unable to get result for GetManagedObjects\n");
+
+    if (result) {
+        g_print("\n\nUsing \"GetAll\":\n");
+        result = g_variant_get_child_value(result, 0);
+        const gchar* property_name;
+        GVariantIter i;
+        GVariant* prop_val;
+        g_variant_iter_init(&i, result);
+        while (g_variant_iter_next(&i, "{&sv}", &property_name, &prop_val))
+            bluez_property_value(property_name, prop_val);
+        g_variant_unref(prop_val);
+    }
+
+    g_main_loop_quit(loop);
 }
 
 static void bluez_list_controllers(GDBusConnection* con,
     GAsyncResult* res,
     gpointer data)
 {
+    (void)data;
     GVariant* result = NULL;
-    GMainLoop* loop = NULL;
-    gchar* s = NULL;
     GVariantIter i;
     const gchar* object_path;
     GVariant* ifaces_and_properties;
 
-    loop = (GMainLoop*)data;
+    auto loop = (GMainLoop*)data; 
     result = g_dbus_connection_call_finish(con, res, NULL);
     if (result == NULL)
         g_print("Unable to get result for GetManagedObjects\n");
@@ -85,8 +115,16 @@ static void bluez_list_controllers(GDBusConnection* con,
             GVariantIter ii;
             g_variant_iter_init(&ii, ifaces_and_properties);
             while (g_variant_iter_next(&ii, "{&s@a{sv}}", &interface_name, &properties)) {
-                if (g_strstr_len(g_ascii_strdown(interface_name, -1), -1, "adapter"))
-                    g_print("HCI Name: %s Address: %s\n", bluez_adapter_get_property(object_path, "Name"), bluez_adapter_get_property(object_path, "Address"));
+                if (g_strstr_len(g_ascii_strdown(interface_name, -1), -1, "adapter")) {
+                    g_print("[ %s ]\n", object_path);
+                    const gchar* property_name;
+                    GVariantIter iii;
+                    GVariant* prop_val;
+                    g_variant_iter_init(&iii, properties);
+                    while (g_variant_iter_next(&iii, "{&sv}", &property_name, &prop_val))
+                        bluez_property_value(property_name, prop_val);
+                    g_variant_unref(prop_val);
+                }
                 g_variant_unref(properties);
             }
             g_variant_unref(ifaces_and_properties);
@@ -94,39 +132,6 @@ static void bluez_list_controllers(GDBusConnection* con,
         g_variant_unref(result);
     }
     g_main_loop_quit(loop);
-}
-
-static void bluez_property_value(const gchar* key, GVariant* value)
-{
-    const gchar* type = g_variant_get_type_string(value);
-
-    g_print("\t%s : ", key);
-    switch (*type) {
-    case 'o':
-    case 's':
-        g_print("%s\n", g_variant_get_string(value, NULL));
-        break;
-    case 'b':
-        g_print("%d\n", g_variant_get_boolean(value));
-        break;
-    case 'u':
-        g_print("%d\n", g_variant_get_uint32(value));
-        break;
-    case 'a':
-        /* TODO Handling only 'as', but not array of dicts */
-        if (g_strcmp0(type, "as"))
-            break;
-        g_print("\n");
-        const gchar* uuid;
-        GVariantIter i;
-        g_variant_iter_init(&i, value);
-        while (g_variant_iter_next(&i, "s", &uuid))
-            g_print("\t\t%s\n", uuid);
-        break;
-    default:
-        g_print("Other\n");
-        break;
-    }
 }
 
 static void bluez_list_devices(GDBusConnection* con,
@@ -422,6 +427,21 @@ int main(int argc, char** argv)
         -1,
         NULL,
         (GAsyncReadyCallback)bluez_list_controllers,
+        loop);
+
+    g_main_loop_run(loop);
+
+    g_dbus_connection_call(con,
+        "org.bluez",
+        "/org/bluez/hci0",
+        "org.freedesktop.DBus.Properties",
+        "GetAll",
+        g_variant_new("(s)", "org.bluez.Adapter1"),
+        G_VARIANT_TYPE("(a{sv})"),
+        G_DBUS_CALL_FLAGS_NONE,
+        -1,
+        NULL,
+        (GAsyncReadyCallback)bluez_adapter_getall_property,
         loop);
 
     g_main_loop_run(loop);
